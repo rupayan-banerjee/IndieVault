6b8e04f1b783c44af8efa77659b4d9dc981508a3
Rupayan Banerjee <103538229@student.swin.edu.au>
2025-05-24 16:43:19 +1000

    basic setup

--- IndieVault/src/App.vue
+++ IndieVault/src/App.vue
@@ -1,30 +1,24 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container mt-4">
+    <nav class="nav mb-4">
+      <router-link class="nav-link" to="/">Home</router-link>
+      <router-link class="nav-link" to="/news">News</router-link>
+      <router-link class="nav-link" to="/about">About</router-link>
+    </nav>
+
+    <router-view />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+export default {
+  name: 'App'
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+</script>
+
+<style>
+/* Responsive support, minor padding fix */
+.nav-link {
+  font-weight: bold;
 }
 </style>
