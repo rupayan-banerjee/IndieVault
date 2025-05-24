8bbd0a5369b86765dffc199d75d88919468fd2ad
Rupayan Banerjee <103538229@student.swin.edu.au>
2025-05-25 01:54:19 +1000

    login and register pages successfully working

--- IndieVault/src/router/index.js
+++ IndieVault/src/router/index.js
@@ -2,11 +2,15 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import News from '../views/News.vue'
 import About from '../views/About.vue'
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
 
 const routes = [
     { path: '/', name: 'Home', component: Home },
     { path: '/news', name: 'News', component: News },
-    { path: '/about', name: 'About', component: About }
+    { path: '/about', name: 'About', component: About },
+    { path: '/login', name: 'Login', component: Login },
+    { path: '/register', name: 'Register', component: Register }
 ]
 
 const router = createRouter({
